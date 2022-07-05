122f196afb41bd40447cb594a35c6b3e58a6fc20
AnhNhat111 <0306191344@caothang.edu.vn>
2022-07-05 12:06:30 +0700

    fillter

--- api_travel_app/app/Http/Controllers/api_admin/AdminBookingController.php
+++ api_travel_app/app/Http/Controllers/api_admin/AdminBookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api_admin;
 use App\Http\Controllers\Controller;
 use App\Models\booking;
 use App\Models\role;
+use App\Models\tour;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -72,70 +73,5 @@ class AdminBookingController extends Controller
             }
         }
         return response()->json($get_booking);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
