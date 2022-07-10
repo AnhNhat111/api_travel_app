2364d78056448440bb36e42d2cda152014049c13
AnhNhat111 <0306191344@caothang.edu.vn>
2022-07-11 01:16:37 +0700

    fix valid token admin

--- api_travel_app/app/Http/Controllers/api/AuthController.php
+++ api_travel_app/app/Http/Controllers/api/AuthController.php
@@ -212,7 +212,6 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
